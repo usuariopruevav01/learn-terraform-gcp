ff39a9d9d66d32baac4c45f2a9ee89f2cd2323fa
Giancarlo Guerra Farfan <gian.guerra23@gmail.com>
2022-03-22 17:40:50 -0500

    v00

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("gcpservacckeys-terraformpractice.json")
+
+  project = "terraform-gcp-guide"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
